6aff44091fbacf09f277a771221d4d6cdde68531
Swathysuriyababu12 <swathysuryababu@gmail.com>
2022-08-16 13:58:33 +0530

    make it better

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ dotenv.config();
 const cors = require("cors");
 const connectDB = require("./connect");
 const userRouter = require("./routes/users");
-connectDB();
+
 //database connection
-app.use(cors());
-res.header("Access-Control-Allow-Origin");
+
+connectDB();
 
 //midddlewares
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => res.send(`Server Running`));
 app.use("/api/user", userRouter);
 const PORT = process.env.PORT || 8080;
